3a9ba136899a5d8a2a9e73e05d03447a517fdc47
Andrzej Sydor <andrzejsydor@gmail.com>
2020-08-06 12:23:17 +0200

    [Terraform][CloudFront][WAF2] integration

---
+++ Terraform/CloudFront+WAF2/main.tf
@@ -0,0 +1,147 @@
+resource "aws_wafv2_web_acl" "aws_wafv2_web_acl_CLOUDFRONT" {
+  name        = "CLOUDFRONT-managed-rule"
+  description = "Example of a CLOUDFRONT managed rule."
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "rule-2"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+
+        excluded_rule {
+          name = "SizeRestrictions_QUERYSTRING"
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "friendly-rule-metric-name"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "friendly-metric-name"
+    sampled_requests_enabled   = false
+  }
+}
+
+
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = var.s3-bucket-name
+  acl    = "private"
+
+  website {
+    index_document="index.html"
+    error_document="index.html"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_object" "static_website_bucket_object" {
+  key    = "index.html"
+  bucket = aws_s3_bucket.static_website_bucket.id
+  source = "${path.cwd}/index.html"
+
+  content_type = "text/html"
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "sth"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_website_bucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.static_website_bucket.bucket_regional_domain_name
+        origin_id = "S3-${aws_s3_bucket.static_website_bucket.bucket}"
+ 
+        s3_origin_config {
+          origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+        }
+    }
+    # By default, show index.html file
+    default_root_object = "index.html"
+    enabled = true
+
+    # AWS WAF WEB ACL
+    web_acl_id = aws_wafv2_web_acl.aws_wafv2_web_acl_CLOUDFRONT.arn
+
+    # If there is a 404, return index.html with a HTTP 200 Response
+    custom_error_response {
+        error_caching_min_ttl = 3000
+        error_code = 404
+        response_code = 200
+        response_page_path = "/index.html"
+    }
+
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD"]
+        # allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "S3-${aws_s3_bucket.static_website_bucket.bucket}"
+
+        # Forward all query strings, cookies and headers
+        forwarded_values {
+            query_string = true
+
+            cookies {
+                forward = "none"
+            }
+        }
+
+        viewer_protocol_policy = "allow-all"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+    }
+
+    # Distributes content to US and Europe
+    price_class = "PriceClass_100"
+
+    # Restricts who is able to access this content
+    restrictions {
+        geo_restriction {
+            # type of restriction, blacklist, whitelist or none
+            restriction_type = "none"
+        }
+    }
+
+    # SSL certificate for the service.
+    viewer_certificate {
+        cloudfront_default_certificate = true
+    }
+}
---
+++ Terraform/CloudFront+WAF2/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "us-east-1"
+}
+
+variable "s3-bucket-name" {
+  default = "s3-bucket-name-345345dfshtjylk43298erty"
+}
+
+variable "TAG-Project" {
+  default = "test-dynamodb-table"
+}
+
+variable "TAG-Environment" {
+  default = "Sandbox"
+}
